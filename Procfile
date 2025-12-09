fc63bc2833bb6f3af960b0a8defd5b6d3046ab80
Jason Hendriks <jason@hendriks.ca>
2025-12-09 07:28:35 -0500

    Configure for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -cp "build/libs/planningpoker-all.jar" ca.hendriks.planningpoker.MainKt
--- src/main/kotlin/planningpoker/Main.kt
+++ src/main/kotlin/planningpoker/Main.kt
@@ -2,9 +2,9 @@ package ca.hendriks.planningpoker
 
 import ca.hendriks.planningpoker.routing.UserSession
 import ca.hendriks.planningpoker.routing.configureRouting
-import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.netty.EngineMain
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.ktor.server.sessions.SessionStorageMemory
 import io.ktor.server.sessions.Sessions
 import io.ktor.server.sessions.cookie
@@ -12,16 +12,15 @@ import io.ktor.server.sse.SSE
 import io.ktor.server.webjars.Webjars
 
 fun main(args: Array<String>) {
-    EngineMain.main(args)
-}
-
-fun Application.module() {
-    install(Webjars) {
-        path = "assets"
-    }
-    install(SSE)
-    install(Sessions) {
-        cookie<UserSession>("user_session", SessionStorageMemory())
-    }
-    configureRouting()
+    val externalPort = System.getProperty("server.port", "8888").toInt()
+    embeddedServer(Netty, port = externalPort) {
+        install(Webjars) {
+            path = "assets"
+        }
+        install(SSE)
+        install(Sessions) {
+            cookie<UserSession>("user_session", SessionStorageMemory())
+        }
+        configureRouting()
+    }.start(wait = true)
 }
