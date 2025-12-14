e517fbcd6aeda293591bfac29838d4cd67adeecf
Jason Hendriks <jason@hendriks.ca>
2025-12-14 04:31:27 -0500

    @Suite recommended I use @SelectPackages instead of @SelectClasspathResource

--- src/intTest/kotlin/ca/hendriks/kotlinpoker/cucumber/CucumberIT.kt
+++ src/intTest/kotlin/ca/hendriks/kotlinpoker/cucumber/CucumberIT.kt
@@ -4,12 +4,12 @@ import io.cucumber.junit.platform.engine.Constants
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.ConfigurationParameters
 import org.junit.platform.suite.api.IncludeEngines
-import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.Suite
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("specification")
+@SelectPackages("specification")
 @ConfigurationParameters(
     ConfigurationParameter(
         key = Constants.PLUGIN_PROPERTY_NAME,
